10539a9c07a42280a7fe0431e40cc626e72e04c7
Steven <etiennehunziker@gmail.com>
2025-10-29 11:58:24 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE = 'chrono-pwa-v1';
+const FILES = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    )).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  if (url.origin === location.origin) {
+    e.respondWith(
+      caches.match(e.request).then(res => {
+        return res || fetch(e.request).catch(() => caches.match('/index.html'));
+      })
+    );
+  }
+});
